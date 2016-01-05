8c662e1d424724ecb52b36adfddf45e4fe60283c
egervais7 <edward.a.gervais89@gmail.com>
2016-01-04 17:08:19 -0800

    Added validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,15 @@
+class LinkValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
+      record.errors[attribute] << (options[:message] || "is not a link")
+    end
+  end
+end
+
+
 class Post < ActiveRecord::Base
+  validates :title, presence: true, length: {in: 10..100}
+  validates :link, presence: true, link: true
+
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not an email")
+    end
+  end
+end
+
 class User < ActiveRecord::Base
-  validate :email, presence: true, uniqueness: {case_sensitive: false}
-  :name, presence: true, uniqueness: {length: <20}
+  validates :email, presence: true, email: true, uniqueness: {case_sensitive: false}
+  validates :name, presence: true, length: {maximum: 20}
 
   has_secure_password
 end
